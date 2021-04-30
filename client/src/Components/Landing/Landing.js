3945ec0f71803ffb49cf77c05c0c3fb96b3341a5
Samaresh Samanta <ss9261@srmist.edu.in>
2021-04-30 22:33:09 +0530

    added the networking----connected the backend to the frontend

--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -1,11 +1,52 @@
-import react from 'react'
+import react, { useEffect, useState } from 'react'
 import classes from "./Landing.module.css";
+import axios from "axios";
 
 const Landing = ()=>{
+
+    const [file,setFile] = useState(null);
+    const [text,setText] = useState(null);
+
+
+
+    useEffect(()=>{console.log(file)},[file])
+
+/*
+    fetch('http://127.0.0.1:5000/upload').then(
+        res=>console.log(res)
+    )
+*/
+
+
+        const handleSubmit = ()=>{
+
+        const formData = new FormData();
+
+        formData.append('name', 'abc123');
+        formData.append('file', file);
+
+        fetch('http://127.0.0.1:5000/upload', {
+        method: 'POST',
+        body: formData
+        })
+        .then(result => {
+            result.json().then(
+                res => setText(res.data)
+            )
+        })
+        .catch(error => {
+        console.error('Error:', error);
+        });
+
+    }
+
+
+
     return(
         <div data-testid="Landing">
-            <input type="file" />
-            <button>submits</button>
+            <input type="file" onChange={(file)=>{setFile(file.target.files[0])}} />
+            <button onClick={handleSubmit}>submits</button>
+            {text&& <p>{text}</p>}
         </div>
     )
 }
