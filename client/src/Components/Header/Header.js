24f166986e9ebd29625b57556a142f7d35d1d8be
Samaresh Samanta <ss9261@srmist.edu.in>
2021-05-15 17:39:54 +0530

    add upload page with animation, adjusted header

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -6,9 +6,15 @@ import { Link } from 'react-router-dom';
 const Header = ()=>{
     return(
         <div className={styles.Nav}>
-            <p id={styles.heading1}>Quizify</p>
-            <motion.p whileHover={{scale:1.04,textShadow:"0px 0px 2px #fff"}}><Link className={styles.heading2}>Home</Link></motion.p>
-            <motion.p whileHover={{scale:1.04,textShadow:"0px 0px 2px #fff"}}><Link className={styles.heading2}>About Us</Link></motion.p>
+            <div className={styles.logoContainer}>
+                <p id={styles.heading1}>Quizify</p>
+            </div>
+            <div className={styles.navBar}>
+                <ul>
+                    <motion.li whileHover={{scale:1.04}}><Link className={styles.heading2}>Home</Link></motion.li>
+                    <motion.li whileHover={{scale:1.04}}><Link className={styles.heading2}>About Us</Link></motion.li>
+                </ul>
+            </div>
         </div>
     )
 }
